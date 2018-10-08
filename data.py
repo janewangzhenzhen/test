1e63d0eae3ea5bbcdae3821b382b44fe2c5b5480
janewangzhenzhen <wangzhenzhen1021@gmail.com>
2018-10-08 10:35:50 +0800

    initial project version

---
+++ data.py
@@ -0,0 +1,16 @@
+def Articles():
+	articles = [
+		{
+		'id': 1,
+		'title': 'Article One',
+		'body': 'lorem	lorem',
+		'author': 'Jane Wang',
+		},
+		{
+		'id': 2,
+		'title': 'Article Two',
+		'body': 'happy happy',
+		'author': 'Jone Snow'
+		}
+	]
+	return articles
